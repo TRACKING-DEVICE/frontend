0c2796375b07cbd14d3674301988ba4fba47d3a5
scriptkid-ke <mwangialex813@gmail.com>
2025-06-24 11:31:59 +0300

    Navbar mobile responsive

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../src/assets/logo.png"
 import search from "../../src/assets/searchIcon.svg"
+import menu from "../../src/assets/menuIcon.svg"
+import close from "../../src/assets/closeIcon.svg"
 
 
 const Navbar = () => {
@@ -54,20 +56,13 @@ const Navbar = () => {
 
                 {/* Mobile Menu Button */}
                 <div className="flex items-center gap-3 md:hidden">
-                    <svg onClick={() => setIsMenuOpen(!isMenuOpen)} className={`h-6 w-6 cursor-pointer ${isScrolled ? "invert" : ""}`} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                        <line x1="4" y1="6" x2="20" y2="6" />
-                        <line x1="4" y1="12" x2="20" y2="12" />
-                        <line x1="4" y1="18" x2="20" y2="18" />
-                    </svg>
+                    <img onClick={()=>setIsMenuOpen(!isMenuOpen)} src={menu} alt="menu-icon" />
                 </div>
 
                 {/* Mobile Menu */}
                 <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
-                        <svg className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                            <line x1="18" y1="6" x2="6" y2="18" />
-                            <line x1="6" y1="6" x2="18" y2="18" />
-                        </svg>
+                        <img src={close} alt="closeicon" className="h-6"/>
                     </button>
 
                     {navLinks.map((link, i) => (
