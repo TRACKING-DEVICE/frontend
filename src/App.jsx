3caabd9e2d44fff80a7e1a2631886245cec7ab9b
scriptkid-ke <mwangialex813@gmail.com>
2025-07-01 10:55:43 +0300

    Moved files for Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
+import Dashboard from './components/Dashboard';
+import BudgetDetail from './components/BudgetDetail';
+import ExpenseForm from './components/ExpensesForm';
+
+
+
+
+
 
 
 
@@ -14,10 +22,18 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar /> }
+      {/* <Dashboard /> */}
       <div className='min-h-[70vh]'>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+              <Route path='/' element={<Home/>}/>
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/budget/:id" element={<BudgetDetail />} />
+              {/* <Route path="/expenses" element={<ExpensesPage />} />
+              <Route path='/expenselist' element={<ExpenseList />} /> */}
         </Routes>
+        <ExpenseForm />
+        <Dashboard />
+        
       </div>
     </div>
   );
